01b77e964b5a10e28a2685416ca7bff49a0f4e0f
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-09-06 14:29:24 +0530

    chore: initialize nextjs app

---
+++ apps/frontend/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from 'next-themes';
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
+
+const inter = Inter({
+    weight: ['400', '500', '600', '700', '800', '900'],
+    subsets: ["latin"],
+    display: 'swap'
+});
+
+export const metadata: Metadata = {
+    title: "Letter Terminal",
+    description: "A note-taking app.",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+    return (
+        <html lang="en">
+            <head>
+                <meta name="apple-mobile-web-app-title" content="Letter Terminal" />
+            </head>
+
+            <body className={`${inter.className}`}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                >
+                    {children}
+                </ThemeProvider>
+
+                <Analytics />
+            </body>
+        </html>
+    );
+}
